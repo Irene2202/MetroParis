0486b7953163ad2c0a24aacfc0c579fc0b7715dc
Irene Baldassa <s257470@studenti.polito.it>
2021-04-26 11:36:31 +0200

    Soluzione Lezione 26/04

--- src/main/java/it/polito/tdp/metroparis/db/MetroDAO.java
+++ src/main/java/it/polito/tdp/metroparis/db/MetroDAO.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 import com.javadocmd.simplelatlng.LatLng;
 
+import it.polito.tdp.metroparis.model.Connessione;
 import it.polito.tdp.metroparis.model.Fermata;
 import it.polito.tdp.metroparis.model.Linea;
 
@@ -66,6 +67,73 @@ public class MetroDAO {
 		}
 
 		return linee;
+	}
+
+	public boolean fermateCollegate(Fermata f1, Fermata f2) {
+		
+		String sql="SELECT COUNT(*) AS cnt "
+				+"FROM connessione c "
+				+"WHERE (id_stazP=? AND id_stazA=?) OR (id_stazP=? AND id_stazA=?)";
+		
+		try {
+			Connection conn=DBConnect.getConnection();
+			PreparedStatement st=conn.prepareStatement(sql);
+			st.setInt(1, f1.getIdFermata());
+			st.setInt(2, f2.getIdFermata());
+			st.setInt(3, f2.getIdFermata());
+			st.setInt(4, f1.getIdFermata());
+			
+			ResultSet rs=st.executeQuery();
+			
+			rs.first();
+			
+			int conteggio=rs.getInt("cnt");
+			
+			conn.close();
+			
+			return (conteggio>0);
+		} catch (SQLException e) {
+			throw new RuntimeException("Errore DB", e);
+		}
+	}
+	
+	public List<Connessione> getAllConnessioni(List<Fermata> fermate){
+		String sql="SELECT id_connessione, id_linea, id_stazP, id_stazA "
+				+"FROM connessione "
+				+"WHERE id_stazP>id_stazA"; //uso > così dimezzo dim tabella (Ho tante andate quanti ritorni)
+		
+		try {
+			Connection conn=DBConnect.getConnection();
+			PreparedStatement st=conn.prepareStatement(sql);
+			ResultSet rs=st.executeQuery();
+			
+			List<Connessione> result=new ArrayList<>();
+			while(rs.next()) {
+				int id_partenza=rs.getInt("id_stazP");
+				Fermata f_part=null;
+				for(Fermata f:fermate)
+					if(f.getIdFermata()==id_partenza)
+						f_part=f;
+				int id_arrivo=rs.getInt("id_stazA");
+				Fermata f_arr=null;
+				for(Fermata f:fermate)
+					if(f.getIdFermata()==id_arrivo)
+						f_arr=f;
+
+				Connessione c=new Connessione(rs.getInt("id_connessione"),
+						null, //ignoro perchè adesso non mi serve
+						f_part, //Mi serve OGGETTO fermata e non solo id
+						f_arr
+						);
+				result.add(c);
+			}
+			
+			conn.close();
+			return result;
+		} catch(SQLException e) {
+			throw new RuntimeException("Errore DB", e);
+		}
+		
 	}
 
 
---
+++ src/main/java/it/polito/tdp/metroparis/model/Model.java
@@ -0,0 +1,53 @@
+package it.polito.tdp.metroparis.model;
+
+import java.util.List;
+
+import org.jgrapht.Graph;
+import org.jgrapht.Graphs;
+import org.jgrapht.graph.DefaultEdge;
+import org.jgrapht.graph.SimpleGraph;
+
+import it.polito.tdp.metroparis.db.MetroDAO;
+
+public class Model {
+
+	Graph<Fermata, DefaultEdge> grafo;
+	
+	public void creaGrafo() {
+		this.grafo=new SimpleGraph<>(DefaultEdge.class); 
+		
+		MetroDAO dao= new MetroDAO();
+		List<Fermata> fermate=dao.getAllFermate();
+		
+		
+//		for(Fermata f: fermate) {
+//			this.grafo.addVertex(f);
+//		}
+		
+		Graphs.addAllVertices(this.grafo, fermate);
+		
+		//Aggiungo archi
+		//Metodo con doppio loop è poco pratico quando ho tanti vertici
+		//è ok se ho un grafo piccolo
+		/*
+		for(Fermata f1:this.grafo.vertexSet()) {
+			for(Fermata f2: this.grafo.vertexSet()) {
+				//Tra queste due stazioni devo mettere arco?
+				//Se se diversi, posso fare query a Dao chiedendo se sono collegate
+				//facendo query ripetute ci metterò una vita ad eseguire
+				//ne stiamo provando troppe
+				if(!f1.equals(f2) && dao.fermateCollegate(f1, f2)) {
+					this.grafo.addEdge(f1, f2);
+				}
+			}
+		}*/
+		
+		List<Connessione> connessioni=dao.getAllConnessioni(fermate);
+		for(Connessione c:connessioni)
+			this.grafo.addEdge(c.getStazP(), c.getStazA());
+		
+		System.out.println(this.grafo);
+		
+	}
+	
+}
